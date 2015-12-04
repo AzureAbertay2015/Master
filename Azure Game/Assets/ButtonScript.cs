4a0a66ed51c1b1dec2e901fcf1d5ba3a8eac23a3
Peter Terrell <terrelllo@outlook.com>
2015-12-04 14:52:39 +0000

    asdf

---
+++ Azure Game/Assets/ButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+    public GameObject Door;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player") ;
+            //StartCoroutine(Door.GetComponent(DoorScript).OpenUp());
+    }
+}
---
+++ Azure Game/Assets/DoorScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+    /*
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    */
+    public Vector3 OpenPosition;
+    private bool open;
+
+    public IEnumerator OpenUp()
+    {
+        if (!open)
+        {
+            while(transform.position!=OpenPosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, OpenPosition, 0.25f);
+                if (Vector3.Distance(transform.position, OpenPosition) <= 0.25f)
+                {
+                    transform.position = OpenPosition;
+                    open = true;
+                }
+                yield return null;
+            }
+        }
+    }
+}
