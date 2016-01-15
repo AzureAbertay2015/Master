bfa897340c8cf1401210dd0244b9b8ec19540fba
Finlay <thumpster72@gmail.com>
2016-01-15 16:04:19 +0000

    Big Merge
    Merged lots of stuff

---
+++ Azure Game/Assets/Azure Game/Scripts/Player/cameraScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour
+{
+
+    float lockPos = 0;
+    private GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(lockPos, 270, lockPos);
+        transform.position = new Vector3(10, player.transform.position.y, player.transform.position.z + 10);
+    }
+}
