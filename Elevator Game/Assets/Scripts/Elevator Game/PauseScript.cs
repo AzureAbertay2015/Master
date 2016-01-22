ed23d1b2170c63e5b352bb36d40c4edfa0cc4367
Finlay <thumpster72@gmail.com>
2016-01-22 15:56:19 +0000

    Menu and timer added
    Menu and Timer added

---
+++ Elevator Game/Assets/Scripts/Elevator Game/PauseScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+    public void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            //save data
+            
+            //pause the game
+            Application.LoadLevel("Menu Scene");
+        }
+    }
+}
