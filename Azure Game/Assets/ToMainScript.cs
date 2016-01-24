2f1d06d4858c6db0cf0ba759366b49a2eb4f1689
Peter Terrell <terrelllo@outlook.com>
2016-01-24 18:05:12 +0000

    implemented pause menu
    resume game/exit to main.

---
+++ Azure Game/Assets/ToMainScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToMainScript : MonoBehaviour {
+
+    public void nextScene()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Menu Scene");
+    }
+}
